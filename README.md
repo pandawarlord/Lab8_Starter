ba34ca2d4a45bfa9e50f5913a6ac77a182ff27b9
Khanh Le <Lekhanh2003@gmail.com>
2023-05-28 22:44:41 -0700

    done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab 8 - Starter
 1. you would do option 2 which is doing it manually before pushing it as you're still adjusting and fixing code as you're going along so it wouldn't make sense to push it if you don't know if it works or not. 
 2. yes
+3. Yes because we want to test if the message is valid ie. if we want a filter or not to test for profanity if we don't want it
+4. yes we want to test all possible too long inputs for edge case ie, we don't count spaces, or just 1 above it etc.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('Dashes and Parenthesis', () => {
+    expect(functions.isPhoneNumber('(999)999-9999')).toBe(true);
+});
+
+test('Not Number', () => {
+    expect(functions.isPhoneNumber('cum')).toBe(false);
+});
+
+test('Long', () => {
+    expect(functions.isPhoneNumber('9999999999999999999999999999-9999999999999999999999999999999999')).toBe(true);
+});
+
+test('Too Short', () => {
+    expect(functions.isPhoneNumber('99')).toBe(false);
+});
+
+test('My Email', () => {
+    expect(functions.isEmail('kvle@ucsd.edu')).toBe(true);
+});
+    
+test('underscore', () => {
+    expect(functions.isEmail('teee_heee@gmail.com')).toBe(true);
+});
+    
+test('Not .com', () => {
+    expect(functions.isEmail('hi@Im.abouttobussssss')).toBe(false);
+});
+    
+test('No @', () => {
+    expect(functions.isEmail('ballshehehe')).toBe(false);
+});
+
+test('Upper, special character, and number', () => {
+    expect(functions.isStrongPassword('P3N!5')).toBe(false);
+});
+        
+test('No Lowercase', () => {
+    expect(functions.isStrongPassword('PP@PPPP@$PP')).toBe(false);
+});
+        
+test('Only Lowercase', () => {
+    expect(functions.isStrongPassword('balls')).toBe(true);
+});
+        
+test('Only Uppercase', () => {
+    expect(functions.isStrongPassword('BALLS')).toBe(true);
+});
+
+test('Shortened Date', () => {
+    expect(functions.isDate('2/9/02')).toBe(false);
+});
+    
+test('Furture date', () => {
+    expect(functions.isDate('02/09/2033')).toBe(true);
+});
+    
+test('Past Date', () => {
+    expect(functions.isDate('02/09/2003')).toBe(true);
+});
+    
+test('Letters', () => {
+    expect(functions.isDate('A/B/C')).toBe(false);
+});
+
+test('Real Hex', () => {
+    expect(functions.isHexColor('#000001')).toBe(true);
+});
+
+test('Short', () => {
+    expect(functions.isHexColor('666')).toBe(true);
+});
+
+test('Too Long', () => {
+    expect(functions.isHexColor('99999999999999999999999')).toBe(false);
+});
+
+test('Word', () => {
+    expect(functions.isHexColor('MUC')).toBe(false);
+});
